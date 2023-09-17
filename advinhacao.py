2f603f065dcb199e2b904eb13b5feba7178e9e4d
Henrique Marques <henrique.marquesvj@gmail.com>
2023-09-17 18:00:37 -0300

    atribuindo comando continue e break

--- advinhacao.py
+++ advinhacao.py
@@ -7,10 +7,14 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f"Tentativa {rodada} de {total_de_tentativas}.")
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre 1 e 100 : "))
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100....")
+        continue
+    
 
     if acertou:
         print(f"Você acertou")

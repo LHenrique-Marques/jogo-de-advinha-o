5656e3e510a350a8fad718ac7a3420980ca53826
Henrique Marques <henrique.marquesvj@gmail.com>
2023-10-07 17:33:36 -0300

    adicionando escolha de nivel

--- advinhacao.py
+++ advinhacao.py
@@ -5,7 +5,26 @@ print("""
 **********************************************************
 """)
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+
+print("""
+Qual o nível de Dificuldade ? 
+      Nivel 1 : Facil
+      Nivel 2 : Médio
+      Nivel 3 : Dificil
+""")
+
+nivel = int(input("Escolha:  "))
+
+if (nivel == 1 ):
+    total_de_tentativas = 20
+
+elif (nivel == 2):
+    total_de_tentativas = 10
+
+else:
+    total_de_tentativas = 5
+
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f"Tentativa {rodada} de {total_de_tentativas}.")
@@ -13,19 +32,19 @@ for rodada in range(1, total_de_tentativas + 1):
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
-    if chute < 1 or chute > 100:
+    if (chute < 1 or chute > 100):
         print("Você deve digitar um número entre 1 e 100....")
         continue
     
 
-    if acertou:
+    if (acertou):
         print(f"Você acertou")
         break
     else:
-        if maior:
+        if (maior):
             print(f"Você errou, o seu chute foi maior que o numero secreto.")
 
-        if menor:
+        if (menor):
             print(f"Você errou, seu chute foi menor que o número secreto.")
 
 print(f"Fim do jogo!!, o número era {numero_secreto}")
